d1d313b565f27a65ed1d804933c91a31026b82f1
vijitha9129 <vijitha.kanumuru@gmail.com>
2022-11-26 20:13:09 -0600

    updated

--- image_processing.py
+++ image_processing.py
@@ -8,15 +8,27 @@ Source:
 
 
 from wand.image import Image
+from os import listdir
+from os.path import isfile, join
+import cv2
+import pytesseract
 
-oreo = Image(filename ='SL1500.jpg')
+#oreo = Image(filename ='SL1500.jpg')
 ##print(oreo.height, oreo.width)
 
-def image_resize():
-    resize_oreo=oreo.resize(1000,1000)
-    return oreo.width,oreo.height
 
+
+def get_files():
+    oreo = [f for f in listdir('Images/Oreo') if isfile(join('Images/Oreo', f))]
+    print(oreo)
+    
+
+def image_resize():
+    for img in get_files.oreo:
+        resize_oreo=oreo.resize(1000,1000)
+        return oreo.width,oreo.height
 
 if __name__== "__main__":
-    resize_width,resize_height=image_resize()
-    print(resize_width,resize_height)
+    #resize_width,resize_height=image_resize()
+    #print(resize_width,resize_height)
+    get_files()
